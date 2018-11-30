1e786c120a14a7cb500a0065bbb1fe1f11c30a68
Rahkill <rahkill@gmail.com>
2018-11-30 17:52:11 -0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const bot =  new Discord.Client();
+const config = require('./config.json');
+const links = require('./links.json');
+
+
+//heroku
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+  // comandos quando o bot estiver pronto
+bot.on('ready', () => {
+    bot.user.setActivity('twitch.tv/yonkjr');
+    console.log('bot logado!');
+});
+
+// comandos de mensagens de usuário
+bot.on('message', message => {
+    if (message.author.bot){
+        return;
+    }
+
+    if (message.content.indexOf(config.prefix) !== 0){
+        return;
+    }
+
+    const msgs = message.content.slice(config.prefix.lenght).trim().split(/ +/g);
+    //transformar os comandos em minusculo
+    const comando = msgs.shift().toLocaleLowerCase();
+
+    responseObject = links;
+    if (responseObject[message.content]){
+        message.channel.send(responseObject[message.content]);
+    }
+
+   
+
+    });
+    
+    
+bot.login(config.token);
